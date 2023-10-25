8ee5408f1f5cd0623805636328d713fc6c6b870a
Büşra <124040084+busraerim@users.noreply.github.com>
2023-10-25 11:33:47 +0300

    Add files via upload

---
+++ travioapp/travioapp/travioapp/InputBox.swift
@@ -0,0 +1,87 @@
+//
+//  InputBox.swift
+//  travioapp
+//
+//  Created by Büşra Erim on 15.10.2023.
+//
+
+import UIKit
+import SnapKit
+import TinyConstraints
+
+enum TitlePlaceholderText {
+    case label(label: String?)
+    case placeholder(placeholder: String?)
+    
+    var text:String?{
+        switch self {
+        case .label(let label):
+            return label
+        case .placeholder(let placeholder):
+            return placeholder
+        }
+    }
+
+}
+
+class InputBox: UIView {
+    
+    var boxTitle:TitlePlaceholderText = .label(label: " ") {
+        didSet{
+            lblTitle.text = boxTitle.text
+        }
+        
+    }
+
+    var boxPlaceholder:TitlePlaceholderText = .placeholder(placeholder: " "){
+        didSet{
+            txtPlaceholder.placeholder = boxPlaceholder.text
+        }
+    }
+    
+    private lazy var lblTitle:UILabel = {
+        let title = UILabel()
+        title.textColor = .black
+        title.font = UIFont(name: "Avenir-Medium", size: 16)
+        title.width(39)
+        title.height(21)
+        return title
+    }()
+    
+    private lazy var txtPlaceholder: UITextField = {
+       let placeholder = UITextField()
+        placeholder.font = UIFont(name: "Avenir-Medium", size: 14)
+
+        return placeholder
+    }()
+    
+
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        self.layer.cornerRadius = 16
+        self.height(74)
+        self.backgroundColor =  UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.00)
+        self.layer.shadowRadius = 20
+        self.layer.shadowOpacity = 0.15
+        self.addSubviews(lblTitle,txtPlaceholder)
+
+        lblTitle.topToSuperview(offset: 10)
+        lblTitle.leadingToSuperview(offset: 5)
+        lblTitle.trailingToSuperview(offset: 200)
+        
+        txtPlaceholder.topToBottom(of: lblTitle, offset: 6)
+//        txtPlaceholder.centerX(to: lblTitle)
+        txtPlaceholder.leadingToSuperview(offset: 5)
+        txtPlaceholder.trailingToSuperview(offset: 200)
+
+        
+    }
+    
+
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+  //sv.layer.shadowRadius=20
+//sv.layer.shadowopacity = 0.15
---
+++ travioapp/travioapp/travioapp/LoginVc.swift
@@ -0,0 +1,162 @@
+//
+//  ViewController.swift
+//  travioapp
+//
+//  Created by Büşra Erim on 15.10.2023.
+//
+
+import UIKit
+import SnapKit
+import TinyConstraints
+
+class LoginVc: UIViewController {
+
+    private lazy var travioImage:UIImageView = {
+        let image = UIImageView(frame: CGRect(x: 0, y: 0, width: 149, height: 178))
+        image.image = UIImage(named: "travio-logo 1")
+        return image
+    }()
+    
+    private lazy var backView:UIView = {
+       let backView = UIView()
+        backView.backgroundColor = UIColor(red: 0.97, green: 0.97, blue: 0.97, alpha: 1.00)
+       return backView
+    }()
+    
+    private lazy var emailView:UIView = {
+        let email = InputBox()
+        email.boxTitle = .label(label: "Email")
+        email.boxPlaceholder = .placeholder(placeholder: "example@mail.com")
+        return email
+    }()
+    
+    private lazy var passwordView:UIView = {
+        let password = InputBox()
+        password.boxTitle = .label(label: "Password")
+        password.boxPlaceholder = .placeholder(placeholder: "*************")
+        return password
+    }()
+    
+    private lazy var stackView:UIStackView = {
+       let stack = UIStackView()
+        stack.axis = .vertical
+        stack.distribution = .fillEqually
+        stack.spacing = 9
+        return stack
+    }()
+    
+    private lazy var btnLogin:UIButton = {
+        let login = UIButton()
+        login.setTitle("Log in", for: .normal)
+        login.layer.cornerRadius = 10
+        login.height(54)
+        login.backgroundColor = UIColor(red: 0.22, green: 0.68, blue: 0.66, alpha: 1.00)
+        return login
+    }()
+    
+    private lazy var lblAccount:UILabel = {
+        let account = UILabel()
+        account.text = "Don’t have any account?"
+        account.textColor = .black
+        account.numberOfLines = 1
+        account.textAlignment = .center
+        account.font = UIFont(name: "Avenir-Medium", size: 18)
+        return account
+    }()
+    
+    private lazy var btnSignup:UIButton = {
+       let signUp = UIButton()
+        signUp.setTitle("Sign Up", for: .normal)
+        signUp.setTitleColor(.black, for: .normal)
+       return signUp
+    }()
+    
+    private lazy var stackSignup:UIStackView = {
+       let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.spacing = 6
+        return stack
+    }()
+    
+    private lazy var lblWelcome:UILabel = {
+       let welcome = UILabel()
+        welcome.text = "Welcome to Travio"
+        welcome.textColor = .black
+        welcome.font = UIFont(name: "Poppins", size: 24)
+        welcome.width(226)
+        welcome.height(36)
+        welcome.textAlignment = .center
+        return welcome
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 0.22, green: 0.68, blue: 0.66, alpha: 1.00)
+        setupViews()
+    }
+
+    private func setupViews(){
+        self.view.addSubviews(backView,btnLogin)
+        
+        backView.addSubviews(stackView,stackSignup,lblWelcome)
+        stackView.addArrangedSubviews(emailView, passwordView)
+        stackSignup.addArrangedSubviews(lblAccount,btnSignup)
+        self.view.addSubview(travioImage)
+        setupLayout()
+    }
+    
+    private func setupLayout(){
+        
+        backView.edgesToSuperview(excluding: .top)
+        backView.height(598)
+        backView.width(390)
+//        backView.topToSuperview(offset: 246)
+        
+        emailView.width(342)
+        stackView.topToBottom(of: lblWelcome, offset: 20)
+        stackView.leadingToSuperview(offset: 24)
+        stackView.trailingToSuperview(offset: 24)
+        
+        btnLogin.width(342)
+        btnLogin.topToBottom(of: stackView, offset: 60)
+        btnLogin.centerX(to: stackView)
+//        btnLogin.leadingToSuperview(offset: 140)
+//        btnLogin.trailingToSuperview(offset: 140)
+        
+        stackSignup.bottomToSuperview(offset: -15, usingSafeArea: true)
+        stackSignup.centerX(to: stackView)
+        
+      
+//        lblWelcome.top(to: backView, offset: 64)
+//        lblWelcome.leadingToSuperview(offset: 82)
+//        lblWelcome.trailingToSuperview(offset:82)
+        lblWelcome.topToSuperview(offset: 40)
+        lblWelcome.centerX(to: stackView)
+        
+        travioImage.width(149)
+        travioImage.height(178)
+        travioImage.topToSuperview(offset: 44)
+        travioImage.bottomToSuperview(offset: -622)
+        travioImage.horizontalToSuperview(insets: .left(120) + .right(121))
+        
+//        travioImage.bottomToTop(of: backView, offset: -24)
+//        travioImage.leftToSuperview(offset: 120)
+        
+        backView.layoutIfNeeded()
+        backView.roundCorners(corners: .topLeft, radius: 80)
+    }
+
+}
+
+
+#if DEBUG
+import SwiftUI
+
+@available(iOS 13, *)
+struct LoginVc_Preview: PreviewProvider {
+    static var previews: some View{
+         
+        LoginVc().showPreview()
+    }
+}
+#endif
